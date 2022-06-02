4943d5181bb2353d5cea34f183eb3e66328f37e8
Lbyers20 <90797604+Lbyers20@users.noreply.github.com>
2022-06-02 19:02:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,83 +1,25 @@
-var arr;
-var pos; //current position to test
-var sortEnd; //end of the sorted portion
-var insertVal; //value to be added
-
 function setup() {
-  frameRate(5);
   createCanvas(400, 400);
   background(0);
-  arr = new randomArray(10);
-
-  sortEnd = 0;
-  pos = sortEnd + 1;
-  insertVal = arr(pos);
-  // put setup code here
+  fill(255);
+  
+  let radius = 30;
+  let xSpeed = random(1, 5);
+  let ySpeed = random(1, 5);
+  let = random(width/4, 3*width/4);
+  let = random(height/4, 3*height/4);
 }
 
 function draw() {
-    background(0);
-
-  //if sorted, just display. sortEnd is arr.length -1
-  if ( sortEnd == arr.length - 1) {
-    displayArray(arr, -1, -1, -1);
+  background(0);
+  circle(x, y, 2* radius);
+  x += xSpeed;
+  y += ySpeed;
+   
+  if(x >= width - radius || x <= radius){
+    xSpeed *= -1;
   }
-  //else
-  else {
-  displayArray(arr, pos, sortEnd, insertVal);
-    //compare insertVal and arr[pos -1]
-    //if pos is not 0 AND inserVal <
-    if (pos != 0 && insertVal < arr(pos-1) ) {
-      //move value at [pos-1] to [pos]
-      arr[pos] = arr[pos-1];
-      //decrease pos
-      pos--;
-    }
-    //else
-    else {
-      //set arr[pos] to insertVal
-      arr[pos] = insertVal;
-      //reset sortEnd, pos, insertVal
-      sortEnd++;
-      pos = sortEnd + 1;
-      if (pos < arr.length) {
-        insertVal = arr[pos];
-      }
-    }
-  }//unsorted
-  // put drawing code here
+  else if(y >= height - radius || y <= radius){
+    ySpeed *= -1;
+  }
 }
-
-function randomArray(num) {
-  var values = new array(num);
-
-  for (var i=0; i<values.length; i++) {
-    values[i] = int(random(100, 400));
-  }//random value loop
-  return values;
-}//randomArray
-
-function displayArray(arr, p, se, iv) {
-  var barWidth = width / arr.length;
-  var x = 0;
-  var y = 0;
-  noStroke();
-  for (var i=0; i<arr.length; i++) {
-    y = height - arr[i];
-    if (i == p ) {
-      fill(0, 230, 230);
-    }
-    else if (i == se) {
-      fill(230, 0, 230);
-    }
-    else {
-      fill(255);
-    }
-    rect(x, y, barWidth, arr[i]);
-    if (i == p) {
-      fill(230, 230, 0);
-      rect(x, height-iv, barWidth, iv);
-    }
-    x+= barWidth;
-  }
-}//displayArray
